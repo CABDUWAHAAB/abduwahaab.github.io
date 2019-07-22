7744381b74bdc830b4e709e1bea900d611566c75
abdiwahaab <abdiwahaab.abdikarim@hotmail.com>
2019-07-22 17:27:42 +0200

    commit

---
+++ waryaa/header.php.php
@@ -0,0 +1,26 @@
+<div id="menuleft">
+</div>
+<div id="menuitems">
+	<ul>
+		<li><a href=#>Blog</a></li>
+		<li><a href=#>services</a></li>
+		<li><a href=#>About us</a></li>
+		<li><a href=#>Contact</a></li>
+	</ul>
+</div>
+<div id="search">
+	<div id="searchbox">
+		Search...
+	</div>
+</div>
+
+</div>
+</div>
+
+<!--  Content container -->
+<div id="container">
+	<div id="Content">
+		<div id="post">
+			<h2>How to attract more readers to your blog</h2>
+			<hr>
+			<p>Posted 11-7-2019 by Abdi Abdikarim - categorized in: blogging</p>
---
+++ waryaa/index.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+ 	<!-- Page navigation -->
+ <div id="navigationbox">
+ 	<div class="pagenavi">
+ 		<span class="pages">Page 1 of 15</span>
+ 		<span class="current">1</span>
+ 		<a href=# class="page" title="2">2</a>
+ 		<a href=# class="page" title="3">3</a>
+ 		<a href=# class="page" title="4">4</a>
+ 		<a href=# class="page" title="5">5</a>
+ 		<a href=# >&raquo;</a>
+ 		<a href=# class="page" title="10">10</a><span class="extend">...</span>
+ 		<a href=# class="last" title="Last &raque;">Last &raquo;</a>
+ 	</div>
+ </div>
+ </body>
+ </html>
